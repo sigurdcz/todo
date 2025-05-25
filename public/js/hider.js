dc43f0214a0a86dcf9d64d0ccb80f2b63b724f06
sigurd gnezdilov <sigurd.gnezdilov@gmail.com>
2025-05-25 13:13:49 +0200

    Hider to hide WZ ads

---
+++ public/js/hider.js
@@ -0,0 +1,19 @@
+(function () {
+    function hideLastFiveBodyElements() {
+        const bodyChildren = document.body.children;
+        const count = Math.min(5, bodyChildren.length);
+        for (let i = bodyChildren.length - count; i < bodyChildren.length; i++) {
+            bodyChildren[i].style.display = 'none';
+        }
+    }
+
+    function delayedHide() {
+        setTimeout(hideLastFiveBodyElements, 3000); // 3s delay
+    }
+
+    if (document.readyState === 'loading') {
+        document.addEventListener("DOMContentLoaded", delayedHide);
+    } else {
+        delayedHide();
+    }
+})();
